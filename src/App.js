cc49f486176101b10253c396e6ab5939c58b24a0
Zahra Namazi <namazi.q@gmail.com>
2021-11-21 00:53:38 +0330

    prepare app to deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Container, Title, Wrapper } from './components/Swap'
+import { Button } from './components/FormControll'
+import SelectBox from './components/SelectBox'
+import Rate from './components/Rate'
+import { defaultWallets } from './constants/wallets'
+import Api from './utils/axios'
 
-function App() {
+const App = () => {
+  const axios = new Api()
+  const [wallets, setWallets] = React.useState(defaultWallets)
+  const [payerWallet, setPayerWallet] = React.useState(wallets[0])
+  const [receiverWallet, setReceiverWallet] = React.useState(wallets[1])
+  const [rate, setRate] = React.useState()
+  const [amount, setAmount] = React.useState({ pay: '', receive: '' })
+  const [error, setError] = React.useState('')
+
+  React.useEffect(() => {
+    const getRate = async () => {
+      try {
+        let rate = await axios.get(`/${payerWallet.name}`)
+        //
+        setRate(rate.data)
+      } catch (error) {
+        console.log('Error happend in fetching data', error)
+      }
+    }
+    getRate()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [payerWallet])
+
+  React.useEffect(() => {
+    if (rate && amount.pay) handleAmount(amount.pay)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [payerWallet, receiverWallet, rate])
+
+  const changeWallet = (walletName, type) => {
+    let selectedWallet = wallets.find((wallet) => wallet.name === walletName)
+    if (type === 'Payer') {
+      if (selectedWallet.name === receiverWallet.name)
+        setReceiverWallet(payerWallet)
+
+      setPayerWallet(selectedWallet)
+    } else {
+      if (selectedWallet.name === payerWallet.name)
+        setPayerWallet(receiverWallet)
+
+      setReceiverWallet(selectedWallet)
+    }
+  }
+  const handleAmount = (amount, label) => {
+    setError('')
+    let balance =
+      label === 'Payer' ? payerWallet.balance : receiverWallet.balance
+    if (amount > balance) {
+      setError({ message: 'Exceed Balance', label })
+      label === 'Payer'
+        ? setAmount({ pay: amount, receive: 0 })
+        : setAmount({ pay: 0, receive: amount })
+
+      return
+    }
+    if (amount < 0) {
+      setError({ message: 'Wrong Amount', label })
+      return
+    }
+    if (!rate) {
+      setError('Rate Not defiend')
+      return
+    }
+    if (label === 'Payer') {
+      let receive = parseFloat(
+        (rate.conversion_rates[receiverWallet.name] * amount).toFixed(3)
+      )
+      setAmount({ pay: parseFloat(amount), receive })
+    } else {
+      let pay = parseFloat(
+        (amount / rate.conversion_rates[receiverWallet.name]).toFixed(3)
+      )
+      setAmount({ pay, receive: parseFloat(amount) })
+
+      if (pay > payerWallet.balance) {
+        setError({ message: 'Exceed Balance', label: 'Payer' })
+      }
+    }
+  }
+
+  const handleSwap = () => {
+    let swap = payerWallet
+    setPayerWallet(receiverWallet)
+    setReceiverWallet(swap)
+  }
+  const handleExchange = () => {
+    if (error) return
+    payerWallet.balance = parseFloat(
+      (payerWallet.balance - amount.pay).toFixed(3)
+    )
+    receiverWallet.balance = parseFloat(
+      (receiverWallet.balance + amount.receive).toFixed(3)
+    )
+    let wallet = wallets.filter(
+      (item) =>
+        item.name !== payerWallet.name && item.name !== receiverWallet.name
+    )
+    setWallets([...wallet, receiverWallet, payerWallet])
+    setAmount({ pay: '', receive: '' })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrapper>
+      <Title margin="20px">Currency Exchange</Title>
+      <Container maxWidth="600px">
+        <SelectBox
+          label="Payer"
+          wallets={wallets}
+          defaultWallet={payerWallet}
+          changeWallet={changeWallet}
+          handleAmount={handleAmount}
+          amount={amount.pay}
+          error={error}
+        />
+        {rate && (
+          <Rate
+            rate={rate}
+            receiver={receiverWallet}
+            payer={payerWallet}
+            handleSwap={handleSwap}
+          />
+        )}
+        <SelectBox
+          label="Receiver"
+          wallets={wallets}
+          defaultWallet={receiverWallet}
+          changeWallet={changeWallet}
+          handleAmount={handleAmount}
+          error={error}
+          amount={amount.receive}
+        />
+        <Button background="#864879" margin="30px 0 0" onClick={handleExchange}>
+          Exchange
+        </Button>
+      </Container>
+    </Wrapper>
+  )
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,111 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react'
+import App from './App'
+import userEvent from '@testing-library/user-event'
+import { defaultWallets } from './constants/wallets'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+beforeEach(async () => {
+  render(<App />)
+  const rate = await screen.findByText(/\$ 1 = € 0.8832/)
+  expect(rate).toBeInTheDocument()
+})
+
+test('render title app', async () => {
+  const title = screen.getByText(/Currency Exchange/i)
+  expect(title).toBeInTheDocument()
+})
+
+test('calculate amount exchange with amount pay', async () => {
+  const input = await screen.findByLabelText('Payer-input')
+
+  userEvent.clear(input)
+  userEvent.type(input, '10')
+  const output = await screen.findByLabelText('Receiver-input')
+
+  expect(input).toHaveValue(10)
+  expect(output).toHaveValue(8.832)
+})
+
+test('calculate amount exchange with amount receive', async () => {
+  const input = await screen.findByLabelText('Receiver-input')
+
+  userEvent.clear(input)
+  userEvent.type(input, '10')
+  const output = await screen.findByLabelText('Payer-input')
+
+  expect(input).toHaveValue(10)
+  expect(output).toHaveValue(11.322)
+})
+
+test('Exceed Balance', async () => {
+  const input = await screen.findByLabelText('Payer-input')
+
+  userEvent.clear(input)
+  userEvent.type(input, '500')
+  const error = await screen.findByText('Exceed Balance')
+
+  expect(error).toBeInTheDocument()
+})
+
+test('Dont accpect negative number', async () => {
+  const input = await screen.findByLabelText('Payer-input')
+
+  userEvent.clear(input)
+  userEvent.type(input, '-5')
+  const error = await screen.findByText('Wrong Amount')
+
+  expect(error).toBeInTheDocument()
+})
+
+test('exchange', async () => {
+  const defaultWallets = [
+    { name: 'USD', balance: 200, currency: '$' },
+    { name: 'EUR', balance: 150, currency: '€' },
+    { name: 'GBP', balance: 10, currency: '£' }
+  ]
+  const input = await screen.findByLabelText('Payer-input')
+
+  userEvent.clear(input)
+  userEvent.type(input, '10')
+
+  const buttonExchange = screen.getByRole('button', { name: 'Exchange' })
+
+  userEvent.click(buttonExchange)
+  const balancePayer = await screen.findByText(
+    `Balance: ${defaultWallets[0].balance - 10} ${defaultWallets[0].name}`
+  )
+  expect(balancePayer).toBeInTheDocument()
+  let balance = parseFloat((defaultWallets[1].balance + 10 * 0.8832).toFixed(3))
+  const balanceReceiver = await screen.findByText(
+    `Balance: ${balance} ${defaultWallets[1].name}`
+  )
+  expect(balanceReceiver).toBeInTheDocument()
+})
+
+test('should change balance when wallet change and Get new rate based on wallet', async () => {
+  const wallets = screen.getByLabelText('Payer')
+
+  // change Wallet
+  userEvent.selectOptions(wallets, ['GBP'])
+  expect(wallets).toHaveValue('GBP')
+
+  // receive new rate
+  const rate = await screen.findByText(/\£ 1 = € 1.1887/)
+  expect(rate).toBeInTheDocument()
+
+  // //   show balance new Wallet
+  const balance = await screen.findByText(
+    `Balance: ${defaultWallets[2].balance} ${defaultWallets[2].name}`
+  )
+  expect(balance).toBeInTheDocument()
+})
+
+test('should swap', async () => {
+  const swap = screen.getByLabelText('swap')
+
+  userEvent.click(swap)
+
+  const payerWallet = await screen.findByLabelText('Payer')
+  const receiverWallet = await screen.findByLabelText('Receiver')
+  expect(payerWallet).toHaveValue('EUR')
+  expect(receiverWallet).toHaveValue('USD')
+})
